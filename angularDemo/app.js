3a6fbdafc754e8d4e247d46c9654fd98815da3a0
xiaolong <634894289@qq.com>
2017-02-20 11:30:31 +0800

    change

--- angularDemo/app.js
+++ angularDemo/app.js
@@ -18,45 +18,13 @@ angular.module('myApp', ['ui.bootstrap', 'ui.router'])
 			})
 	})
 	.controller('TabsDemoCtrl', function($scope, $window) {
-		$scope.tabs = [{
-			title: 'Dynamic Title 1',
-			content: 'Dynamic content 1'
-		}, {
-			title: 'Dynamic Title 2',
-			content: 'Dynamic content 2',
-			disabled: true
-		}];
-		$scope.alertMe = function() {
-			setTimeout(function() {
-				$window.alert('You\'ve selected the alert tab!');
-			});
-		};
-		$scope.model = {
-			name: 'Tabs'
-		};
+
 	})
 	.controller('ButtonsCtrl', function($scope) {
-		$scope.singleModel = 1;
-		$scope.radioModel = 'Middle';
-		$scope.checkModel = {
-			left: false,
-			middle: true,
-			right: false
-		};
-		$scope.checkResults = [];
-		$scope.$watchCollection('checkModel', function() {
-			$scope.checkResults = [];
-			angular.forEach($scope.checkModel, function(value, key) {
-				if(value) {
-					$scope.checkResults.push(key);
-				}
-			});
-		});
+
 	})
 	.controller('CollapseDemoCtrl', function($scope) {
-		$scope.isNavCollapsed = true;
-		$scope.isCollapsed = false;
-		$scope.isCollapsedHorizontal = false;
+		
 	})
 
 ;
